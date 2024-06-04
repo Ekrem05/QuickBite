b1c0a2bf678b35c2f9cbb46df5d0859fbe68ac2c
Ekrem05 <ekremnptg@gmail.com>
2024-06-04 20:13:10 +0300

    Error handling in next.js

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not found!</h1>
+      <p>Unfortunatelly, we could not found this page</p>
+    </main>
+  );
+}
